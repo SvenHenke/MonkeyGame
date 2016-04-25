2df46be8dbd3d22c2da6277df6257479c1a37e8e
SvenHenke <svenhe@imac-2.local>
2016-04-25 09:01:09 +0200

    Upload

--- SpaceMonkey/GameScene.swift
+++ SpaceMonkey/GameScene.swift
@@ -7,6 +7,21 @@
 //
 
 import SpriteKit
- 
+
 class GameScene: SKScene {
+    
+    // 1 - Create the sprite
+    let player = SKSpriteNode(imageNamed:"spacemonkey_fly02")
+    
+    override func didMoveToView(view: SKView) {
+        
+        // 2 - Position the sprite
+        player.position = CGPoint(x:frame.size.width * 0.1, y: frame.size.height * 0.5)
+        // 3 - Add the sprite to the scene
+        addChild(player)
+        
+        // 4 - Set scene background color to black
+        backgroundColor = SKColor.blackColor()
+    }
+    
 }
