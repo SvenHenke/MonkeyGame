1959ba9003424344dfb8ba3b1f634eb60d09a97f
SvenHenke <svenhe@imac-2.local>
2016-04-25 08:50:20 +0200

    Initial Commit

---
+++ SpaceMonkey/GameScene.swift
@@ -0,0 +1,12 @@
+//
+//  GameScene.swift
+//  SpaceMonkey
+//
+//  Created by Main Account on 4/10/15.
+//  Copyright (c) 2015 Razeware LLC. All rights reserved.
+//
+
+import SpriteKit
+ 
+class GameScene: SKScene {
+}
